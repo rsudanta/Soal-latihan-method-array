fd5a1c771b2a791d1253c34df9f6e16fde3bcc28
rsudanta <rsudanta20@gmail.com>
2024-09-05 15:55:12 +0700

    homework

--- homework.js
+++ homework.js
@@ -1,56 +1,58 @@
 const videoData = [
-  {
-    name: 'Miss Scarlet',
-    present: true,
-    rooms: {
-      kitchen: false,
-      ballroom: false,
-      conservatory: false,
-      'dining room': false,
-      'billiard room': false,
-      library: false
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: {
+            kitchen: false,
+            ballroom: false,
+            conservatory: false,
+            'dining room': false,
+            'billiard room': false,
+            library: false
+        }
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: {
+            kitchen: false,
+            ballroom: false,
+            conservatory: false,
+            'dining room': false,
+            'billiard room': false,
+            library: false
+        }
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: {
+            kitchen: false,
+            ballroom: false,
+            conservatory: false,
+            'dining room': false,
+            'billiard room': false,
+            library: false
+        }
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: {
+            kitchen: false,
+            ballroom: false,
+            conservatory: false,
+            'dining room': false,
+            'billiard room': false,
+            library: false
+        }
     }
-  },
-  {
-    name: 'Rusty',
-    present: false,
-    rooms: {
-      kitchen: false,
-      ballroom: false,
-      conservatory: false,
-      'dining room': false,
-      'billiard room': false,
-      library: false
-    }
-  },
-  {
-    name: 'Colonel Mustard',
-    present: true,
-    rooms: {
-      kitchen: false,
-      ballroom: false,
-      conservatory: false,
-      'dining room': false,
-      'billiard room': false,
-      library: false
-    }
-  },
-  {
-    name: 'Professor Plum',
-    present: true,
-    rooms: {
-      kitchen: false,
-      ballroom: false,
-      conservatory: false,
-      'dining room': false,
-      'billiard room': false,
-      library: false
-    }
-  }
 ]
 
 let result = videoData.filter((element) => {
-  return element.present
+    return element.present
 })
 
-console.log(result)
+
+console.log("count of result: ", result.length)
+console.log("result: ", result)
