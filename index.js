649c3ef347f1fc40a3e575ef078c7f3883f18e0f
FebrianAditya <febrian.aditya@student.upi.edu>
2024-09-05 10:53:15 +0700

    soal 1

---
+++ index.js
@@ -0,0 +1,12 @@
+const collectionNumber = [1, 2, 3, 4, 5, 6]
+/**
+ * Manipulasi array sederhana
+ * Buat array baru berisi angka2 yg merupakan hasil perkalian 3 dari setiap element array collectionNumber
+ */
+
+
+
+const colors = ["blue", "black", "purple", "white", "pink"]
+/**
+ * buat array baru yang isinya element yang hurufnya lebih dari 4 berdasarkan array colors
+ */
