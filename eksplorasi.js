c7645d08b98b6fece495cebee6c7a8295d90254c
rsudanta <rsudanta20@gmail.com>
2024-09-05 15:53:55 +0700

    homework

--- eksplorasi.js
+++ eksplorasi.js
@@ -5,13 +5,13 @@ let fruits = ['apple', 'banana', 'orange'];
 console.log("fruits: ", fruits);
 
 let students = [{
-        name: 'Alice',
-        grade: 90
-    },
-    {
-        name: 'Bob',
-        grade: 85
-    },
+    name: 'Alice',
+    grade: 90
+},
+{
+    name: 'Bob',
+    grade: 85
+},
 ];
 
 numbers.push(7, 8);
@@ -25,12 +25,12 @@ console.log("shift fruits: ", fruits);
 fruits.unshift('grape', 'kiwi');
 console.log("unsfhit fruits: ", fruits);
 
-numbers.splice(2, 3); 
-console.log("spliced numbers: ",numbers);
+numbers.splice(2, 3);
+console.log("spliced numbers: ", numbers);
 
 let slicedFruits = fruits.slice(1, 3);
 
-console.log("sliced fruits: ",slicedFruits);
+console.log("sliced fruits: ", slicedFruits);
 
 
 let evenNumbers = numbers.filter(num => num % 2 === 0);
@@ -42,3 +42,23 @@ let sortedNumber = numbers.reverse()
 console.log("even number: ", evenNumbers);
 console.log("excellent students: ", excellentStudents);
 console.log("reverse number: ", reversedNumber);
+
+
+let student = {
+    firstName: 'John',
+    lastName: 'John',
+}
+
+let { firstName: studentName } = student
+console.log("destructuring object: ", studentName)
+
+let person = {
+    name: "Anisa",
+    age: 20,
+    increment: function () {
+        this.age += 1
+    }
+}
+
+person.increment()
+console.log(person.age)
